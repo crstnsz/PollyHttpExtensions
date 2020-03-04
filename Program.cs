a07dda084a88a5e433492b75afce24ee6fdb8250
Cristiano Souza <crstnsz@gmail.com>
2020-03-04 19:53:33 -0300

    Teste Polly Extensions

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PollyHttpExtensions
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var httpCliente = new HttpClient() { Timeout = TimeSpan.FromMinutes(1) };
+
+
+            var retryConfiguration = HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .Or<TaskCanceledException>()
+                    .WaitAndRetryAsync(new TimeSpan[] { TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(12), TimeSpan.FromMinutes(1) } );
+
+            int i = 3;
+
+            var teste = await retryConfiguration.ExecuteAndCaptureAsync(async () => await httpCliente.GetAsync("https://localhost:44320/testes-espera?minutos=1"));
+            if (teste.Outcome == OutcomeType.Successful)
+                Console.WriteLine(await teste.Result.Content.ReadAsStringAsync());
+            else
+                Console.WriteLine("Erro");
+            Console.ReadLine();
+        }
+    }
+}
